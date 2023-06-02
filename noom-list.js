ba89f30d1212178a873d7568629446818a3e5bb7
wichaisw <wichai.sw19@gmail.com>
2023-06-03 03:16:25 +0700

    fix: noom page

--- noom-list.js
+++ noom-list.js
@@ -1,10 +1,12 @@
 const mySongs = [
   {
+    id: 1,
     title: "MCR 1",
     link: "https://example.com/song1",
     lyrics: "Lyrics for Song 1",
   },
   {
+    id: 2,
     title: "Keane 1",
     link: "https://example.com/song1",
     lyrics: "Lyrics for Song 1",
